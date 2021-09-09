4cb3780048f6e7c3df43945c78b53fdf9d548a63
kabu1 <bensonmk99@gmail.com>
2021-09-09 11:25:48 +0300

    add angular service

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -6,22 +6,7 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-  recipes: Recipe[] =[
-    {
-      id: 'r1',
-      title: 'Schnitzel',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Schnitzel.JPG/1024px-Schnitzel.JPG',
-      ingredients: ['French Fries','Pork Meat','Salad']
 
-
-  },
-    {
-        id: 'r2',
-        title: 'Spaghetti',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/9/98/Spaghetti-prepared.jpg',
-        ingredients: ['spaghetti', 'Meat', 'Tomatoes']
-    }
-  ];
 
   constructor() { }
 
