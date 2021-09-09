156db3fa236be7ec53ffd4a7a55006681e48f637
kabu1 <bensonmk99@gmail.com>
2021-09-09 09:27:24 +0300

    add recipes

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from './recipe.model';
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-  recipes: Recipe [] =[
+  recipes: Recipe[] =[
     {
       id: 'r1',
       title: 'Schnitzel',
-      imageUrl: http
+      imageUrl: 'https://en.wikipedia.org/wiki/Schnitzel#/media/File:Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+      ingredients: ['French Fries','Pork Meat','Salad']
 
 
-
-  }
-  ]
+  },
+    {
+        id: 'r2',
+        title: 'Spaghetti',
+        imageUrl: 'https://en.wikipedia.org/wiki/Spaghetti#/media/File:Spaghettata.JPG',
+        ingredients: ['spaghetti', 'Meat', 'Tomatoes']
+    }
+  ];
 
   constructor() { }
 
