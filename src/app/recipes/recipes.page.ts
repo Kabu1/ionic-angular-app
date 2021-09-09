a38fac63c8e61b6babe3e97c8e0234e357f4f6b6
kabu1 <bensonmk99@gmail.com>
2021-09-09 09:37:59 +0300

    Change the url for images

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -10,7 +10,7 @@ export class RecipesPage implements OnInit {
     {
       id: 'r1',
       title: 'Schnitzel',
-      imageUrl: 'https://en.wikipedia.org/wiki/Schnitzel#/media/File:Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Schnitzel.JPG/1024px-Schnitzel.JPG',
       ingredients: ['French Fries','Pork Meat','Salad']
 
 
@@ -18,7 +18,7 @@ export class RecipesPage implements OnInit {
     {
         id: 'r2',
         title: 'Spaghetti',
-        imageUrl: 'https://en.wikipedia.org/wiki/Spaghetti#/media/File:Spaghettata.JPG',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/9/98/Spaghetti-prepared.jpg',
         ingredients: ['spaghetti', 'Meat', 'Tomatoes']
     }
   ];
